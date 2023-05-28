fc66c25ca4ee562ee8b7b215f836c72fa1163f83
Gokul Ganesh I <igokulganesh@gmail.com>
2023-05-28 23:59:08 +0530

    Skills Section Updated

--- app/src/components/Skills.jsx
+++ app/src/components/Skills.jsx
@@ -1,32 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Button } from 'primereact/button';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation, Keyboard, EffectCreative } from "swiper";
+import { Autoplay, Pagination, Navigation, Keyboard } from "swiper";
+import imageSkill from "../assets/images/skills.jpg";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "swiper/css/effect-creative";
 
 import "../assets/css/skills.css";
 
-
-let COLOR_INDEX = 0;
-
-const getColor = () => {
-  const colors = ["secondary", "success", "info", "warning", "help", "danger"];
-  COLOR_INDEX = (COLOR_INDEX + 1) % colors.length;
-  return colors[COLOR_INDEX];
-}
-
 const skills = [
-  { group: "Programming Languages", items: ["C", "C++", "Python", "Rust", "Java Script", "Java", "SQL"] },
-  { group: "Concepts", items: ["Cryptography", "Data Structure", "Algorithm", "Database design", "Object Oriented Programming"] },
-  { group: "Web Designing Languages", items: ["React JS", "Redux", "HTML", "CSS", "Jquery", "Bootstrap & Grid System"] },
-  { group: "Frameworks", items: ["Fast Api", "Django", "Rest Api"] },
-  { group: "Tools & Utilities", items: ["Git", "VS Code", "Visual Studio", "Jira", "Bitbucket", "Shell Script", "Jupyter Notebook"] },
-  { group: "Database System", items: ["PostgreSQL", "MySQL", "MS SQL Server"] },
-  //{ group: "Operating System", items: ["Linux", "Windows"] },
+  { group: "Programming Languages", color: "blue", items: ["C", "C++", "Python", "Rust", "Java Script", "Java", "SQL"] },
+  { group: "Concepts", color: "green", items: ["Cryptography", "Data Structure", "Algorithm", "Database design", "Object Oriented Programming"] },
+  { group: "Web Designing Languages", color: "red", items: ["React JS", "Redux", "HTML", "CSS", "Jquery", "Bootstrap & Grid System"] },
+  { group: "Frameworks", color: "yellow", items: ["Fast Api", "Django", "Rest Api"] },
+  { group: "Tools & Utilities", color: "teal", items: ["Git", "VS Code", "Visual Studio", "Jira", "Bitbucket", "Shell Script", "Jupyter Notebook"] },
+  { group: "Database System", color: "pink", items: ["PostgreSQL", "MySQL", "MS SQL Server"] },
+  { group: "Operating System", color: "orange", items: ["Linux", "Windows"] },
 ];
 
 function Skills() {
@@ -46,62 +37,54 @@ function Skills() {
   }, []);
 
   return (
-    <div id="skills" className="m-5" style={{ minHeight: "55vh" }}>
-      <h4 className="mt-6 mb-3 flex align-items-center justify-content-center font-bold text-blue-900 underline">Relevant Skills</h4>
-      <br />
-      <>
-        <Swiper
-          autoplay={{
-            delay: 3500,
-            disableOnInteraction: true,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          loop={true}
-          slidesPerView={1}
-          navigation={!isSmallScreen}
-          modules={[Autoplay, Pagination, Navigation, Keyboard, EffectCreative]}
-          className="mySwiper"
-          keyboard={true}
-          effect={"creative"}
-          creativeEffect={{
-            prev: {
-              shadow: false,
-              translate: [0, 0, -400],
-            },
-            next: {
-              translate: ["100%", 0, 0],
-            },
-          }}
-        >
-          {
-            skills.map(skill => {
-              return (
-                <SwiperSlide key={skill.group}>
-                  <div>
-                    <div className="card p-3 align-items-center justify-content-center" style={{ minHeight: "350px", width: "350px" }}>
-                      <div className="m-2 flex font-medium text-lg text-gray-800">
-                        {skill.group}
+    <div id="skills" className="m-5">
+      <h4 className="flex align-items-center justify-content-center font-bold text-blue-900 underline">Relevant Skills</h4>
+      <div className='flex justify-content-center flex-wrap  align-items-center gap-3'>
+        <div className=''>
+          <img src={imageSkill} alt='skill' height={"100px"} className='responsive-image' />
+        </div>
+        <div>
+          <div style={{ width: "650px" }} className="">
+            <Swiper
+              autoplay={{
+                delay: 3500,
+                disableOnInteraction: true,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              loop={true}
+              slidesPerView={1}
+              navigation={!isSmallScreen}
+              modules={[Autoplay, Pagination, Navigation, Keyboard]}
+              className="mySwiper"
+              keyboard={true}
+            >
+              {
+                skills.map(skill => {
+                  return (
+                    <SwiperSlide key={skill.group}>
+                      <div style={{ height: "250px" }} className={`p-5 ml-5 bg-${skill.color}-100`}>
+                        <p className={`flex font-bold text-lg text-${skill.color}-700`}>{skill.group}</p>
+                        <div className="flex flex-wrap gap-2" style={{ width: "400px" }}>
+                          {skill.items.map(item => {
+                            return (
+                              <div key={item}>
+                                <Button label={item} raised outlined size="small" className={`text-${skill.color}-500`} />
+                              </div>
+                            )
+                          })}
+                        </div>
                       </div>
-                      <div className="flex flex-wrap justify-content-center gap-2">
-                        {skill.items.map(item => {
-                          return (
-                            <div key={item}>
-                              <Button label={item} raised outlined severity={getColor()} size="small" />
-                            </div>
-                          )
-                        })}
-                      </div>
-                    </div>
-                    <br /><br />
-                  </div>
-                </SwiperSlide>
-              );
-            })
-          }
-        </Swiper>
-      </>
+                      <br />
+                    </SwiperSlide>
+                  );
+                })
+              }
+            </Swiper>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
