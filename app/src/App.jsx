8ad69b6c1015fb7f75d7c0b5a25aa86537f7b2b4
Gokul Ganesh I <igokulganesh@gmail.com>
2024-03-15 23:36:55 +0530

    Updated CSS Styles

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div style={{}}>
+    <div className="w-full overflow-hidden">
       <Navbar />
       <Hero />
       <Experience />
